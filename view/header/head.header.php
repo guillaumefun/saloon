1a89dfd5f51486ea1e29eb44f407b3c41504aefd
johnuser <johnskiski@gmail.com>
2017-04-10 19:18:37 +0200

    Commit 4 : ajout de couilles animées
    +création stylesheet.css, ajout fichiers bootstra, ajout fichier
    jquerry.

--- view/header/head.header.php
+++ view/header/head.header.php
@@ -4,5 +4,5 @@
  -->
 <link href="/Saloon/view/resources/bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="/Saloon/view/css/stylesheet.css">
-<script src="/Saloon/view/resources/jquery-1.11.3.min.js"></script>
+<script src="/Saloon/view/resources/jquery-3.2.0.min.js"></script>
 <script src="/Saloon/view/resources/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
--- view/index.php
+++ view/index.php
@@ -1,72 +1,80 @@
-<?php 
+<?php
 
-	session_start();
-	if(isset($_SESSION['id'])){
-		header('Location: dashboard/');
-	}
+session_start();
+if(isset($_SESSION['id'])){
+	header('Location: dashboard/');
+}
 
- ?>
+?>
 <!DOCTYPE html>
 
 <html>
-	<head>
-		<?php include('header/head.header.php'); ?>
-	</head>
+<head>
+	<?php include('header/head.header.php'); ?>
+</head>
 
-	<body>
+<body>
 
-		<div class="container-fluid">
+	<div class="container-fluid">
 
-			<form action="../controller/login.controller.php" method="post">
+		<form action="../controller/login.controller.php" method="post">
+
+			<div class="row gif">
+				<div class="col-md-4 col-md-offset-4">
+					<img class="img-responsive img-circle" src="https://j.gifs.com/98zQDJ.gif">
+				</div>
+			</div>
 
 			<div class="row form-group">
 
-				<div class="col-md-4"></div>
-				<div class="col-md-4">
-					
+				<div class="col-md-5"></div>
+				<div class="col-md-2">
+
+					<div class="input-group">
+						<span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
 						<input type="text" name="login" class="form-control" placeholder="Login" >
-					
+					</div>
+
 				</div>
 
 			</div>
 
 			<div class="row form-group">
 
-				<div class="col-md-4"></div>
-				<div class="col-md-4">
+				<div class="col-md-2 col-md-offset-5">
 
+					<div class="input-group">
+						<span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
 						<input type="password" name="password" class="form-control" >
-					
+					</div>
 				</div>
 
 			</div>
 
 			<div class="row form-group">
 
-				<div class="col-md-4"></div>
-				<div class="col-md-4">
-					
-						<input type="submit" class="btn btn-primary">
+				<div class="col-md-2 col-md-offset-5">
 
-					
+					<input type="submit" class="btn btn-primary">
+
 				</div>
 
 			</div>
 
-			</form>
+		</form>
 
-			<div class="row form-group">
+		<div class="row form-group">
 
-				<div class="col-md-4"></div>
-				<div class="col-md-4">
-						
-						<a href="register.view.php"><button class="btn btn-default">Pas encore inscris ?</button></a>
-					
-				</div>
+
+			<div class="col-md-2 col-md-offset-5">
+
+				<a href="register.view.php"><button class="btn btn-default">Pas encore inscris ?</button></a>
 
 			</div>
 
 		</div>
 
-	</body>
 
+	</div>
+
+</body>
