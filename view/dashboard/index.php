6ba697b03a770ed4a548be7ad90bffe353db937d
johnuser <johnskiski@gmail.com>
2017-04-11 22:54:23 +0200

    Commit 6: Structure de base ok, reste plus qu'à designer le tout
    slider sur la gauche, pop up pour ajout memebre, pop up pour créer
    projet perso. Content principal = LE SALOON et ses projets!

--- view/dashboard/index.php
+++ view/dashboard/index.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
@@ -9,44 +9,87 @@ if(!isset($_SESSION['id'])){
 	header('Location: ../');
 }
 
-  ?>
+?>
 <!DOCTYPE html>
 
 <html>
-	<head>
-		<title>Saloon</title>
-		<?php include('../header/head.header.php'); ?>
-	</head>
+<head>
+	<title>Saloon</title>
+	<?php include('../header/head.header.php'); ?>
+	<link rel="stylesheet" type="text/css" href="/Saloon/view/css/structure.css">
+	<link rel="stylesheet" type="text/css" href="/Saloon/view/css/styleDashboard.css">
+</head>
 
-	<body>
+<body>
 
-		<?php include('../header/navbar.header.php'); ?>
 
-		<div class="container-fluid">
+	<div id="wrapper">
+		<!--FUCKING SIDEBAR-->
+		<div id="sidebar-wrapper">
+			<?php include('all_saloons.php'); ?>
+			<div class="logBtn">
+				<a href="/Saloon/controller/logout.controller.php" class=""><button class="btn btn-primary">Déconnection</button></a>
+			</div>
+		</div><!--end sidebar-wrapper-->
 
-			<div class="row">
-
-				<div class="col-md-3">
-
-					<?php include('all_saloons.php'); ?>
-
+		<!--FUCKING PAGE CONTENT-->
+		<div id="page-content-wrapper">
+			<div class="container-fluid">
+				<div id="rideau" style="display:none; cursor: pointer;"></div> <!--Fond noir quand pop up-->
+				<a href="#menu-toggle" class="btn btn-default" id="menu-toggle">Toggle Menu</a>
+				<div class="row">
+					<div class="col-md-8 col-md-offset-2">
+						<a onclick="HelpBox('creerProjet')" class="btn btn-default">Porte tes couilles et crée un new project!</a>
+						<a onclick="HelpBox('ajoutMembre')" class="btn btn-default">Ajoute des membres à ton Saloon <3</a>
+					</div>
 				</div>
 
-				<div class="col-md-9">
-					<?php include('messages.php'); ?>
-					<?php include('saloon.php'); ?>
+				<div class="row">
+
+					<div class="col-md-8 col-md-offset-2">
+						<?php include('messages.php'); ?>
+						<?php include('saloon.php'); ?>
+					</div>
+
 				</div>
 
 			</div>
-
 		</div>
+	</div><!-- endwrapper-->
 
-	</body>
+
+	<script> /*FUCKING POP UP SCRIPT*/
+	function HelpBox(id){
+		if(document.getElementById(id).style.display!='block'){
+			document.getElementById(id).style.display = 'block';
+		}else{document.getElementById(id).style.display = 'none'}
+		if(document.getElementById("rideau").style.display!='block'){
+			document.getElementById("rideau").style.display = 'block';
+		}else{document.getElementById("rideau").style.display = 'none'}
+		ReHelpBox(id);
+	}
+	function ReHelpBox(id){
+		rideau.addEventListener('click', function(){
+			HelpBox(id);
+		}) ;
+	}
+	</script>
+
+
+
+	<script>  /*MENU TOGGLE SCRIPT*/
+	$("#menu-toggle").click(function(e) {
+		e.preventDefault();
+		$("#wrapper").toggleClass("toggled");
+	});
+	</script>
+
+</body>
 
 <script src="../javascript/rewards.js"></script>
 <script>
 $(document).ready(function(){
-    $('[data-toggle="tooltip"]').tooltip({html: true}); 
+	$('[data-toggle="tooltip"]').tooltip({html: true});
 });
 </script>
 </html>
--- view/dashboard/saloon.php
+++ view/dashboard/saloon.php
@@ -1,46 +1,54 @@
 
 <?php $saloon_id = (isset($_GET['s'])) ? $_GET['s'] : $default_saloon; ?>
 
-<form action="../../controller/bet.controller.php?id=<?php  echo $saloon_id; ?>" method="post">
-	<div class="row form-group">
-		<div class="col-md-4">
-			<label>Nom</label>
-			<input type="text" class="form-control" placeholder="Mon Projet de fou !" name="name">
-		</div>
+<div id="creerProjet" style="display: none">
+	<div id="creerProjetB">
+		<form action="../../controller/bet.controller.php?id=<?php  echo $saloon_id; ?>" method="post">
+			<div class="row form-group">
+				<div class="col-md-4">
+					<label>Nom</label>
+					<input type="text" class="form-control" placeholder="Mon Projet de fou !" name="name">
+				</div>
+			</div>
+			<div class="row form-group">
+				<div class="col-md-10">
+					<label>Description</label>
+					<input type="text" class="form-control" placeholder="La description de mon Projet de fou !" name="description">
+				</div>
+			</div>
+			<div class="row form-group">
+				<div class="col-md-4">
+					<label>Deadline</label>
+					<input type="text" class="form-control" placeholder="25/03/2100" name="deadline">
+				</div>
+			</div>
+			<div class="row form-group">
+				<div class="col-md-4">
+					<input type="submit" class="btn btn-primary" value="Créer un nouveau projet">
+				</div>
+			</div>
+		</form>
 	</div>
-	<div class="row form-group">
-		<div class="col-md-10">
-			<label>Description</label>
-			<input type="text" class="form-control" placeholder="La description de mon Projet de fou !" name="description">
-		</div>
-	</div>
-	<div class="row form-group">
-		<div class="col-md-4">
-			<label>Deadline</label>
-			<input type="text" class="form-control" placeholder="25/03/2100" name="deadline">
-		</div>
-	</div>
-	<div class="row form-group">
-		<div class="col-md-4">
-			<input type="submit" class="btn btn-primary" value="Créer un nouveau projet">
-		</div>
-	</div>
-</form>
+</div>
 
-<form action="../../controller/add_member.controller.php?s=<?php  echo $saloon_id; ?>" method="post">
-	<div class="row form-group">
-		<div class="col-md-4">
-			<label>Ajouter des nouveaux membres</label>
-			<h6>Séparez leur nom d'utilisateur par une virgule</h6>
-			<input type="text" class="form-control" placeholder="dupont365,maxlamenace" name="logins">
-		</div>
+<div id="ajoutMembre" style="display: none">
+	<div id="ajoutMembreB">
+		<form action="../../controller/add_member.controller.php?s=<?php  echo $saloon_id; ?>" method="post">
+			<div class="row form-group">
+				<div class="col-md-4">
+					<label>Ajouter des nouveaux membres</label>
+					<h6>Séparez leur nom d'utilisateur par une virgule</h6>
+					<input type="text" class="form-control" placeholder="dupont365,maxlamenace" name="logins">
+				</div>
+			</div>
+			<div class="row form-group">
+				<div class="col-md-4">
+					<input type="submit" class="btn btn-primary" value="Ajouter des nouveaux membres">
+				</div>
+			</div>
+		</form>
 	</div>
-	<div class="row form-group">
-		<div class="col-md-4">
-			<input type="submit" class="btn btn-primary" value="Ajouter des nouveaux membres">
-		</div>
-	</div>
-</form>
+</div>
 
 <?php
 
@@ -62,144 +70,144 @@ foreach ($saloon as $member) {
 	foreach ($member['bets'] as $bet ) {
 		?>
 
-			<div class="well">
-				<div class="row">
+		<div class="well">
+			<div class="row">
 
-					<div class="col-md-12">
-						<h4><?php echo $bet['name']; ?></h4>
-					</div>
-
-				</div>
-				<div class="row">
-
-					<div class="col-md-12">
-						<h6>Deadline <?php echo $bet['deadline']; ?></h6>
-					</div>
-
+				<div class="col-md-12">
+					<h4><?php echo $bet['name']; ?></h4>
 				</div>
 
-				<div class="row">
+			</div>
+			<div class="row">
 
-					<div class="col-md-12">
-						<h5><?php echo $bet['description']; ?></h5>
-					</div>
-
+				<div class="col-md-12">
+					<h6>Deadline <?php echo $bet['deadline']; ?></h6>
 				</div>
+
 			</div>
 
-			<!-- Récompenses -->
-
 			<div class="row">
+
+				<div class="col-md-12">
+					<h5><?php echo $bet['description']; ?></h5>
+				</div>
+
+			</div>
+		</div>
+
+		<!-- Récompenses -->
+
+		<div class="row">
 			<?php
 
-				$rewards = getRewardsByBetID( $bet['id'] );
-				$count_rewards = 0;
-				foreach( $rewards as $reward ){
-					$count_rewards++;
+			$rewards = getRewardsByBetID( $bet['id'] );
+			$count_rewards = 0;
+			foreach( $rewards as $reward ){
+				$count_rewards++;
 
-					$reward_detail = getRewardDetail( $reward['id'] );
-					$reward_quantity = getRewardQuantity( $reward_detail );
-					?>
-
-						<div class="col-md-2">
-							<?php echo $reward['name'] . "  "; ?><a href="#" data-toggle="tooltip" title="<?php echo printRewardDetail( $reward_detail ); ?>" id="quantity_<?php echo $reward['id']; ?>"><?php echo $reward_quantity; ?></a>
-							<button class="btn btn-default plus" value="<?php echo $reward['id']; ?>"><span class="glyphicon glyphicon-plus"></span></button>
-							<button class="btn btn-default minus" value="<?php echo $reward['id']; ?>"><span class="glyphicon glyphicon-minus"></span></button>
-						</div>
-
-					<?php
-
-				}
+				$reward_detail = getRewardDetail( $reward['id'] );
+				$reward_quantity = getRewardQuantity( $reward_detail );
 				?>
 
-			</div>
-
-			<div class="row">
-
-				<div class="col-md-4">
-						<a href="../rewards.view.php?bet_id=<?php echo $bet['id'] ."&s=" . $saloon_id . "&user_id=" . $bet['user']; ?>"><button class="btn btn-default">Ajouter une récompense</button></a>
+				<div class="col-md-2">
+					<?php echo $reward['name'] . "  "; ?><a href="#" data-toggle="tooltip" title="<?php echo printRewardDetail( $reward_detail ); ?>" id="quantity_<?php echo $reward['id']; ?>"><?php echo $reward_quantity; ?></a>
+					<button class="btn btn-default plus" value="<?php echo $reward['id']; ?>"><span class="glyphicon glyphicon-plus"></span></button>
+					<button class="btn btn-default minus" value="<?php echo $reward['id']; ?>"><span class="glyphicon glyphicon-minus"></span></button>
 				</div>
-			</div>
 
+				<?php
 
-
-			<!-- Commentaires -->
-
-			<?php 
-
-				$comments = getCommentsByBetID( $bet['id'] );
-
+			}
 			?>
 
-			<div class="panel-group">
-			  <div class="panel panel-default">
-			    <div class="panel-heading">
+		</div>
+
+		<div class="row">
+
+			<div class="col-md-4">
+				<a href="../rewards.view.php?bet_id=<?php echo $bet['id'] ."&s=" . $saloon_id . "&user_id=" . $bet['user']; ?>"><button class="btn btn-default">Ajouter une récompense</button></a>
+			</div>
+		</div>
+
+
+
+		<!-- Commentaires -->
+
+		<?php
+
+		$comments = getCommentsByBetID( $bet['id'] );
+
+		?>
+
+		<div class="panel-group">
+			<div class="panel panel-default">
+				<div class="panel-heading">
 					<h4 class="panel-title">
 						<a data-toggle="collapse" href="#comments<?php echo $bet['id']; ?>">Commentaires <span class="badge"><?php echo count($comments); ?></span></a>
 					</h4>
 				</div>
 				<div id="comments<?php echo $bet['id']; ?>" class="panel-collapse collapse">
-      				<div class="panel-body">
+					<div class="panel-body">
 
-			<?php
+						<?php
 
-				foreach ($comments as $comment) {
-					
-					?>
+						foreach ($comments as $comment) {
 
-					<div class="row">
+							?>
 
-						<div class="col-md-8">
-							<h5><?php echo $comment['user_name']; ?></h5>
-							<p><?php echo $comment['content']; ?></p>
-						</div>
+							<div class="row">
 
-					</div>
+								<div class="col-md-8">
+									<h5><?php echo $comment['user_name']; ?></h5>
+									<p><?php echo $comment['content']; ?></p>
+								</div>
 
-					<?php
+							</div>
 
-				}
+							<?php
 
-				if( count($comments) == 0 ){
-					?>
+						}
 
-						<p>Il n'y a pas encore de commentaires</p>
+						if( count($comments) == 0 ){
+							?>
 
-					<?php
-				}
+							<p>Il n'y a pas encore de commentaires</p>
 
-			?>
+							<?php
+						}
+
+						?>
 					</div>
 				</div>
-			  </div>
 			</div>
+		</div>
 
 
-			<form action="../../controller/add_comment.controller.php?b=<?php  echo $bet['id'] . "&s=" . $saloon_id; ?>" method="post">
-				<div class="row form-group">
-					<div class="col-md-4">
-						<input type="text" class="form-control" placeholder="Commentaire" name="comment">
-					</div>
+		<form action="../../controller/add_comment.controller.php?b=<?php  echo $bet['id'] . "&s=" . $saloon_id; ?>" method="post">
+			<div class="row form-group">
+				<div class="col-md-4">
+					<input type="text" class="form-control" placeholder="Commentaire" name="comment">
 				</div>
-				<div class="row form-group">
-					<div class="col-md-4">
-						<input type="submit" class="btn btn-primary" value="Publier">
-					</div>
+			</div>
+			<div class="row form-group">
+				<div class="col-md-4">
+					<input type="submit" class="btn btn-primary" value="Publier">
 				</div>
-			</form>
+			</div>
+		</form>
 
 		<?php
 		$count++;
 	}
 	if($count == 0){
 		?>
-			<div class="row">
+		<div class="row">
 
-				<div class="col-md-5">
-					<p>Cet utilisateur n'a encore rien parié !!</p>
-				</div>
-
+			<div class="col-md-5">
+				<p>Cet utilisateur n'a encore rien parié !!</p>
 			</div>
+
+		</div>
 
 		<?php
 	}
