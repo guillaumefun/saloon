8eb8824823e9ff95b54196627a1323e6f662e798
johnuser <johnskiski@gmail.com>
2017-04-12 20:09:26 +0200

    Commit 10: Background (à voir si on garde..) + ajout shorctus et searchbar

--- view/header/head.header.php
+++ view/header/head.header.php
@@ -6,3 +6,7 @@
 <link rel="stylesheet" type="text/css" href="/Saloon/view/css/stylesheet.css">
 <script src="/Saloon/view/resources/jquery-3.2.0.min.js"></script>
 <script src="/Saloon/view/resources/bootstrap-3.3.7-dist/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="/Saloon/view/css/structure.css">
+<link rel="stylesheet" type="text/css" href="/Saloon/view/css/styleDashboard.css">
+<link href="https://fonts.googleapis.com/css?family=Inconsolata|Sriracha|Permanent+Marker|Rock+Salt" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Lato:400,400i,700,700i" rel="stylesheet">
